49bf75cb0b270f67c04916379c3cf6416bd20ef5
pixls <tha.pixls@gmail.com>
2022-02-24 16:45:46 -0500

    finished exercises

--- outer_space.sql
+++ outer_space.sql
@@ -7,22 +7,30 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  galaxy_name TEXT NOT NULL
+);
+
+CREATE TABLE stars
+(
+  id SERIAL PRIMARY KEY,
+  star_name TEXT NOT NULL,
+  galaxy INT REFERENCES galaxies ON DELETE NULL,
+);
+
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  orbits_around INT REFERENCES stars ON DELETE NULL
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
-VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  moon_name TEXT NOT NULL,
+  orbits_around INT REFERENCES planets ON DELETE NULL
+);
